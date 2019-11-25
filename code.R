a8e243cf65f292693074ed94e9e8e9bd6dda23e9
shahrukh-ak <shahrukhalamkhan201@gmail.com>
2019-11-25 13:39:04 -0800

    Modifying

--- code.R
+++ code.R
@@ -1,6 +1,7 @@
 data(Default , package="ISLR")
-head(Default)
+attach(Default)
 
+head(Default)
 default student   balance    income
 1      No      No  729.5265 44361.625
 2      No     Yes  817.1804 12106.135
@@ -8,4 +9,6 @@ default student   balance    income
 4      No      No  529.2506 35704.494
 5      No      No  785.6559 38463.496
 6      No     Yes  919.5885  7491.559
+
+plot(balance, default)
      
