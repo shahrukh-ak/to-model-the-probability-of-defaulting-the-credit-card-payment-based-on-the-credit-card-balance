cbc6474503872cf35ec93e86f08fb85fa4634c49
shahrukh-ak <shahrukhalamkhan201@gmail.com>
2019-11-25 13:35:41 -0800

    Adding code

---
+++ code.R
@@ -0,0 +1,11 @@
+data(Default , package="ISLR")
+head(Default)
+
+default student   balance    income
+1      No      No  729.5265 44361.625
+2      No     Yes  817.1804 12106.135
+3      No      No 1073.5492 31767.139
+4      No      No  529.2506 35704.494
+5      No      No  785.6559 38463.496
+6      No     Yes  919.5885  7491.559
+     
